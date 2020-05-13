adbb91cbf9d7e65cc23171ea57c6d61791128e2f
Chris Howle <cthowle@gmail.com>
2020-05-13 15:27:34 +0000

    Latest chat troubleshooting updates

--- app/channels/chat/status_channel.rb
+++ app/channels/chat/status_channel.rb
@@ -4,11 +4,11 @@ class Chat::StatusChannel < ApplicationCable::Channel
   def online
     stop_all_streams
     stream_from "chat::status"
-    #current_user.online
+    current_user.online
   end
 
   def unsubscribed
-    #current_user.offline
+    current_user.offline
     stop_all_streams
   end
 end
